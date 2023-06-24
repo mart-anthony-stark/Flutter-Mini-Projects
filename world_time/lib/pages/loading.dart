3883cc93948ac30e92f4b8228d52833d68ed4d49
mart-anthony-stark <martsalazar0@gmail.com>
2023-06-24 23:51:10 +0800

    Add: get time

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +10,29 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    // Make network request
+    Uri uri = Uri.http('worldtimeapi.org', "/api/timezone/Asia/Manila");
+    Response response = await get(uri);
+    Map data = jsonDecode(response.body);
+
+    // Get properties
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    // Create datetime object
+    DateTime now = DateTime.parse(datetime);
+    now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(body: Text('Loading..'));
