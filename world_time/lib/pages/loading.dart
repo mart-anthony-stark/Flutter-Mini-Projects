ba827201d6348ad897dde55eaf0626d41fb87751
mart-anthony-stark <martsalazar0@gmail.com>
2023-06-25 11:41:41 +0800

    Fix api consume

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Loading extends StatefulWidget {
@@ -12,18 +12,31 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
   void getTime() async {
     // Make network request
-    Uri uri = Uri.http("worldtimeapi.org", "/api/timezone/Asia/Manila");
+    // Uri uri = Uri.parse("http://worldtimeapi.org/api/timezone/Asia/Manila");
+    Uri uri =
+        Uri.parse("https://timeapi.io/api/TimeZone/zone?timeZone=Asia/Manila");
     try {
-      Response response = await get(uri);
+      http.Response response = await http.get(uri);
       Map data = jsonDecode(response.body);
+      print(data);
 
-      // Get properties
-      String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(1, 3);
+      // Using World Time Api
+      // String datetime = data['datetime'];
+      // String offset = data['utc_offset'].substring(1, 3);
 
-      // Create datetime object
-      DateTime now = DateTime.parse(datetime);
-      now.add(Duration(hours: int.parse(offset)));
+      // DateTime now = DateTime.parse(datetime);
+      // now.add(Duration(hours: int.parse(offset)));
+
+      // Using timeapi
+      // // Create datetime object
+
+      int utcOffsetSeconds = data["currentUtcOffset"]["seconds"];
+      Duration offset = Duration(seconds: utcOffsetSeconds);
+
+      String datetimeString = data["currentLocalTime"];
+      DateTime now = DateTime.parse(datetimeString);
+      now.add(offset);
+
       print(now);
     } catch (error) {
       print(error);
