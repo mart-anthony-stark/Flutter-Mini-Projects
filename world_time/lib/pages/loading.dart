f0c3f7409d6eabcb78769e8148a2cf0061be46e8
mart-anthony-stark <martsalazar0@gmail.com>
2023-06-25 14:22:57 +0800

    Add: world time class

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -12,48 +11,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    // Make network request
-    // Uri uri = Uri.parse("http://worldtimeapi.org/api/timezone/Asia/Manila");
-    Uri uri =
-        Uri.parse("https://timeapi.io/api/TimeZone/zone?timeZone=Asia/Manila");
-    try {
-      http.Response response = await http.get(uri);
-      Map data = jsonDecode(response.body);
-      print(data);
+  String time = "Loading...";
 
-      // Using World Time Api
-      // String datetime = data['datetime'];
-      // String offset = data['utc_offset'].substring(1, 3);
+  void setupWorldTime() async {
+    WorldTime wt =
+        WorldTime(location: "Philippines", flag: 'ph.png', url: "Asia/Manila");
 
-      // DateTime now = DateTime.parse(datetime);
-      // now.add(Duration(hours: int.parse(offset)));
-
-      // Using timeapi
-      // // Create datetime object
-
-      int utcOffsetSeconds = data["currentUtcOffset"]["seconds"];
-      Duration offset = Duration(seconds: utcOffsetSeconds);
-
-      String datetimeString = data["currentLocalTime"];
-      DateTime now = DateTime.parse(datetimeString);
-      now.add(offset);
-
-      print(now);
-    } catch (error) {
-      print(error);
-    }
+    await wt.getTime();
+    setState(() {
+      time = ('time: ${wt.time}');
+    });
   }
 
   @override
   void initState() {
     super.initState();
 
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('Loading..'));
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(50.0),
+      child: Text(time),
+    ));
   }
 }
---
+++ world_time/lib/services/world_time.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class WorldTime {
+  late String location;
+  late String time;
+  late String flag; // url to asset flag icon
+  late String url;
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    // Make network request
+    // Uri uri = Uri.parse("http://worldtimeapi.org/api/timezone/Asia/Manila");
+    Uri uri = Uri.parse('https://timeapi.io/api/TimeZone/zone?timeZone=$url');
+    try {
+      http.Response response = await http.get(uri);
+      Map data = jsonDecode(response.body);
+
+      int utcOffsetSeconds = data["currentUtcOffset"]["seconds"];
+      Duration offset = Duration(seconds: utcOffsetSeconds);
+
+      String datetimeString = data["currentLocalTime"];
+      DateTime now = DateTime.parse(datetimeString);
+      now.add(offset);
+
+      // Set time property
+      time = now.toString();
+    } catch (error) {
+      print(error);
+    }
+  }
+}
