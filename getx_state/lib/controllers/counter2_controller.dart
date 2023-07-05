7720514de610a52d9e22c446cd2e554120561f85
mart-anthony-stark <martsalazar0@gmail.com>
2023-07-05 15:54:16 +0800

    Remove todos

--- getx_state/lib/controllers/counter2_controller.dart
+++ getx_state/lib/controllers/counter2_controller.dart
@@ -15,14 +15,12 @@ class Counter2Controller extends GetxController {
 
   @override
   void onInit() {
-    // TODO: implement onInit
     super.onInit();
     increment();
   }
 
   @override
   void onClose() {
-    // TODO: implement onClose
     super.onClose();
 
     cleanupTask();
