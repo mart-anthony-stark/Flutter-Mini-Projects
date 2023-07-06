fe74325caf358fab1699737b9f1dac7486a95889
mart-anthony-stark <martsalazar0@gmail.com>
2023-07-07 01:06:17 +0800

    Add: stateful

--- flashcards/lib/flashcard_view.dart
+++ flashcards/lib/flashcard_view.dart
@@ -10,13 +10,19 @@ class FlashcardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 50,
+      shadowColor: Colors.indigo,
       shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+          borderRadius: BorderRadius.all(Radius.circular(5.0))),
       color: side == "front" ? AppColor.PRIMARY : AppColor.SECONDARY,
       child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(color: Colors.white),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
--- flashcards/lib/main.dart
+++ flashcards/lib/main.dart
@@ -1,14 +1,67 @@
 import 'package:flashcards/common/AppColor.dart';
 import 'package:flashcards/flashcard_view.dart';
+import 'package:flashcards/models/flashcard.dart';
 import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          // Define the default brightness and colors.
+          brightness: Brightness.light,
+          primaryColor: AppColor.PRIMARY,
+
+          // Define the default `TextTheme`. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
+            titleLarge: TextStyle(fontSize: 36, fontStyle: FontStyle.italic),
+            bodyMedium: TextStyle(fontSize: 14, fontFamily: 'Hind'),
+          ),
+        ),
+        home: const Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<Flashcard> flashcards = [
+    Flashcard(
+        question:
+            "A storage format that defines the way data is stored, organized, and manipulated. Some popular categories are Arrays, Trees, and Graphs.",
+        answer: "Data Structure"),
+    Flashcard(
+        question:
+            "Commonly referred to as a collection of items stored at contiguous memory locations. Items stored are of the same type. It organizes data so that a related set of values can be easily sorted or searched.",
+        answer: "Array"),
+    Flashcard(
+        question:
+            "Refers to a linear data structure in which the elements are not necessarily stored in a contiguous manner. It is basically a sequence of nodes, each node points towards the next node forming a chain-like structure.",
+        answer: "Linked List"),
+    Flashcard(
+        question:
+            "Refers to a linear data structure performing operations in a LIFO (Last In First Out) order. Elements can only be accessed, starting from the topmost to the bottom element.",
+        answer: "Stack"),
+    Flashcard(
+        question:
+            "Refers to a linear data structure that performs operations in a FIFO order. The least recently added elements are removed first as opposed to a stack.",
+        answer: "Queue"),
+  ];
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +70,6 @@ class MainApp extends StatelessWidget {
         // Define the default brightness and colors.
         brightness: Brightness.light,
         primaryColor: AppColor.PRIMARY,
-
-        // Define the default font family.
-        // fontFamily: 'Georgia',
 
         // Define the default `TextTheme`. Use this to specify the default
         // text styling for headlines, titles, bodies of text, and more.
@@ -38,14 +88,15 @@ class MainApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const SizedBox(
+              SizedBox(
                   width: 250,
                   height: 250,
                   child: FlipCard(
                     front: FlashcardView(
-                      text: "text",
+                      text: flashcards[_currentIndex].question,
                     ),
-                    back: FlashcardView(text: "back", side: "back"),
+                    back: FlashcardView(
+                        text: flashcards[_currentIndex].answer, side: "back"),
                   )),
               const SizedBox(height: 10),
               Padding(
@@ -54,7 +105,7 @@ class MainApp extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     OutlinedButton(
-                        onPressed: () {},
+                        onPressed: showPreviousCard,
                         child: const Row(
                           children: [
                             Icon(
@@ -71,7 +122,7 @@ class MainApp extends StatelessWidget {
                           ],
                         )),
                     OutlinedButton(
-                        onPressed: () {},
+                        onPressed: showNextCard,
                         child: const Row(
                           children: [
                             Text(
@@ -95,5 +146,19 @@ class MainApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void showNextCard() {
+    setState(() {
+      _currentIndex =
+          _currentIndex < flashcards.length - 1 ? _currentIndex + 1 : 0;
+    });
+  }
+
+  void showPreviousCard() {
+    setState(() {
+      _currentIndex =
+          _currentIndex == 0 ? flashcards.length - 1 : _currentIndex - 1;
+    });
   }
 }
